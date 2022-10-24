cd728ee2bd53c54c027e90ac0ad2eb41ccec7fb9
Dan Koskiranta <G00397054@atu.ie>
2022-10-24 09:53:17 +0100

    Prompt the user for email and course

--- src/main/java/ie/atu/week6/StudentApp.java
+++ src/main/java/ie/atu/week6/StudentApp.java
@@ -6,7 +6,15 @@ public class StudentApp {
     public static void main(String[] args) {
         System.out.println("Please enter your name: ");
         Scanner input = new Scanner(System.in);
-        String name = input.next();
+        String name = input.nextLine();
         System.out.println("Your name is: " + name);
+
+        System.out.println("Please enter your email: ");
+        String eMail = input.nextLine();
+        System.out.println("Your email is: " + eMail);
+
+        System.out.println("Please enter your course: ");
+        String myCourse = input.nextLine();
+        System.out.println("Your course is: " + myCourse);
     }
 }
