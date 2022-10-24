d894cbbbcdff743f695cecc15aa0cbc407f8b526
Dan Koskiranta <G00397054@atu.ie>
2022-10-24 09:43:33 +0100

    Prompt the user for their name

--- src/main/java/ie/atu/week6/StudentApp.java
+++ src/main/java/ie/atu/week6/StudentApp.java
@@ -1,4 +1,12 @@
 package ie.atu.week6;
 
+import java.util.Scanner;
+
 public class StudentApp {
+    public static void main(String[] args) {
+        System.out.println("Please enter your name: ");
+        Scanner input = new Scanner(System.in);
+        String name = input.next();
+        System.out.println("Your name is: " + name);
+    }
 }
