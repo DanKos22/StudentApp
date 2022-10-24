e031efa7b3c454a5b582fd0766c585207c0dfeb8
Dan Koskiranta <G00397054@atu.ie>
2022-10-24 09:59:23 +0100

    Added a new file called student

---
+++ src/main/java/ie/atu/week6/Student.java
@@ -0,0 +1,31 @@
+package ie.atu.week6;
+
+public class Student {
+    private String name;
+    private String eMail;
+    private String myCourse;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String geteMail() {
+        return eMail;
+    }
+
+    public void seteMail(String eMail) {
+        this.eMail = eMail;
+    }
+
+    public String getMyCourse() {
+        return myCourse;
+    }
+
+    public void setMyCourse(String myCourse) {
+        this.myCourse = myCourse;
+    }
+}
