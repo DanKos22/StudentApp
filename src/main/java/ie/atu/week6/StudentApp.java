50cc9d06ee69cb642126ac876094627a77c9d6a3
oskark22 <oskar.koskiranta@gmail.com>
2022-10-25 22:41:35 +0300

    Modified code

--- src/main/java/ie/atu/week6/StudentApp.java
+++ src/main/java/ie/atu/week6/StudentApp.java
@@ -7,6 +7,7 @@ public class StudentApp {
         System.out.println("Please enter your name: ");
         Scanner input = new Scanner(System.in);
         String name = input.nextLine();
+        // First instance of Student class
         Student first_Student = new Student();
         first_Student.setName(name);
         System.out.println("Your name is: " + first_Student.getName());
@@ -21,6 +22,7 @@ public class StudentApp {
         first_Student.setMyCourse(myCourse);
         System.out.println("Your course is: " + first_Student.getMyCourse());
 
+        // Second instance of Student class
         Student second_Student = new Student("John");
         System.out.println("Name of the second student is: " + second_Student.getName());
 
@@ -34,7 +36,7 @@ public class StudentApp {
         second_Student.setMyCourse(myCourse2);
         System.out.println("Course for the 2nd student: " + second_Student.getMyCourse());
 
-        Student third_Student = new Student("Mike", "ie.atu", "Computer science");
+        Student third_Student = new Student("Mike", "G00222444@atu.ie", "Computer science");
         System.out.println("Name of the 3rd student is: " + third_Student.getName());
         System.out.println("Email of the 3rd student is: " + third_Student.geteMail());
         System.out.println("Course of the 3rd student is: " + third_Student.getMyCourse());
